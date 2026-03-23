48d6cdeb8c838ac028a20e206aa8a4aac808d79c
Sanjana H S <sanjugangatkar068@gmail.com>
2026-03-23 21:19:17 +0530

    Update mental  health chatbot.py

--- mental  health chatbot.py
+++ mental  health chatbot.py
@@ -1,5 +1,7 @@
 import streamlit as st
 st.set_page_config(page_title="Mental Health Chatbot", page_icon="🧠")
 st.title("🧠 Mental Health Support Chatbot")
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
 
