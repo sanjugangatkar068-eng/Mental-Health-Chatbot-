a8b593a574b140c8b91ce6eac2ef40b852856e9d
Sanjana H S <sanjugangatkar068@gmail.com>
2026-03-29 19:36:22 +0530

    Update mental  health chatbot.py

--- mental  health chatbot.py
+++ mental  health chatbot.py
@@ -7,5 +7,7 @@ user_input = st.text_input("How are you feeling today?")
 def get_response(text):
     text = text.lower()
 if any(word in text for word in ["sad", "depressed", "low"]):
+          return "I'm really sorry you're feeling this way 💙 Try talking to someone you trust or doing something you enjoy."
+
 
 
